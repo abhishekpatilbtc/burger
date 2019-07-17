19992f056aa36b407f02660bce2aa0a9fa359b90
admin <47126450+abhishekpatilbtc@users.noreply.github.com>
2019-07-17 14:59:59 -0400

    sql schema and seed created

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) values ('Chicken Burger', TRUE);
+
+INSERT INTO burgers (burger_name, devoured) values ('Ham Burger', TRUE);
+
+INSERT INTO burgers (burger_name, devoured) values ('Turkey Burger', TRUE);
