eec4bcf3d81338e22c952076c1310390e7f6a33f
Abhishek Patil <AbhishekPatil@gatech.edu>
2019-07-21 12:11:40 -0400

    updating orm

--- config/orm.js
+++ config/orm.js
@@ -3,34 +3,31 @@ var connection = require("./connection.js");
 // Object Relational Mapper (ORM)
 
 var orm = {
-  selectAll: function(tableInput, colToSearch, valOfCol) {
-    var queryString = "SELECT * FROM ??";
-    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
-      if (err) throw err;
-      console.log(result);
-    });
-  },
-  insertOne: function(whatToSelect, table, orderCol) {
-    var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
-    console.log(queryString);
-    connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
-      if (err) throw err;
-      console.log(result);
-    });
-  },
-  updateOne: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
-    var queryString =
-      "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
 
-    connection.query(
-      queryString,
-      [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
-      function(err, result) {
-        if (err) throw err;
-        console.log(result);
-      }
-    );
+  selectAll: function (tableName, cb) {
+      let queryString = "SELECT * FROM ??";
+      connection.query(queryString, [tableName], (error, results) => {
+          if (error) throw error;
+          // console.log(results)
+          cb(results);
+      })
+  },
+  insertOne: function (tableName, burgerName, cb) {
+      let queryString = "INSERT INTO ??(burger_name) values(?)";
+      connection.query(queryString, [tableName, burgerName], (error, results) => {
+          if (error) throw error;
+          cb(results);
+      })
+  },
+  updateOne: function (tableName, devouredTrue, id, cb) {
+      let queryString = "UPDATE ?? SET ? WHERE ?";
+      let devoured = { devoured: devouredTrue };
+      let idvar = { id: id };
+      connection.query(queryString, [tableName, devoured, idvar], (error, results) => {
+          if (error) throw error;
+          cb(results);
+      })
   }
-};
+}
 
 module.exports = orm;
